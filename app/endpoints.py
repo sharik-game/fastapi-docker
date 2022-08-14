01ee64ae558150091742624e27990c4b7d9363f5
Razorenov Vladimir <vova2008@yandex.ru>
2022-08-14 13:01:01 +0300

    hashing

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,28 +1,20 @@
 from app.db import database, User
 from app.models import UserInDB, UserIn, UserOut
-
+from app.hashing import Hasher
 
 async def user_input(user_in: UserIn):
     user_in2 = user_in.dict()
-    # answer = False
+    email_in_out = await User.objects.get(User.email == user_in2["email"])
+    check_password = Hasher.verify_password(user_in2["hashed_password"], email_in_out.dict()["hashed_password"])
+    if check_password:
+        user_output = UserOut(**email_in_out.dict())
+        return user_output
+    else:
+        raise KeyError
     
-    check = await User.objects.get(User.hashed_password == user_in2["hashed_password"])
-    # email_in_out = await User.objects.get(User.email == user_in2["email"])
-    # price_in_out = await User.objects.get(User.price == user_in2["price"])
-    print(check)
-    user_output = UserOut(**check.dict())
-    return user_output
-    # answer = user_output
-    # return user_output
-    # answer = user_output
-    
-    # return {"input": False}
-    # return answer
         
 
 
 async def user_register(user_in: UserIn):
-    user_in_db = UserInDB(email = user_in.dict()["email"], hashed_password = user_in.dict()["hashed_password"], price = user_in.dict()["price"])
-    # await User.objects.create(email = user_in_db.dict()["email"], hashed_password = user_in_db.dict()["hashed_password"], price = user_in_db.dict()["price"])
+    user_in_db = UserInDB(email = user_in.dict()["email"], hashed_password = Hasher.get_password_hash(user_in.dict()["hashed_password"]), price = user_in.dict()["price"])
     await User.objects.create(**user_in_db.dict())
-    # return {"register": True}
